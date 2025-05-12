681840e71ed564e50d8b50cc02496f6492e0496f
OPdarmawan <op1805@gmail.com>
2025-05-13 01:06:58 +0700

    update layout.js, page.js, team-detail.js and team-listing.js

--- app/team/[slug]/page.js
+++ app/team/[slug]/page.js
@@ -1,25 +1,24 @@
-'use client'
+"use client"
 
-import TeamDetail from "@/components/team-detail";
-import { getItems, getUsers } from "@/lib/api";
-import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
-import Loading from "@/components/loading";
+import TeamDetail from "@/components/team-detail"
+import { getItems, getUsers } from "@/lib/api"
+import { useEffect, useState } from "react"
+import { useParams } from "next/navigation"
+import Loading from "@/components/loading"
 
 export default function TeamMemberPage() {
-  const params = useParams();
-  const { slug } = params;
-  const [firstname, lastname] = slug.split("-");
-  const [agentData, setAgentData] = useState(null);
-  const [agentProperties, setAgentProperties] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const params = useParams()
+  const { slug } = params
+  const [firstname, lastname] = slug.split("-")
+  const [agentData, setAgentData] = useState(null)
+  const [testimonials, setTestimonials] = useState([])
+  const [agentProperties, setAgentProperties] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-
     const fetchDataAgent = async () => {
       try {
-
         const dataTeam = await getUsers({
           fields: ["*.*"],
           filter: {
@@ -31,36 +30,51 @@ export default function TeamMemberPage() {
         // Find the matched agent based on first and last name (case-insensitive, trimmed)
         const matchedAgent = dataTeam?.find(
           (agent) =>
-            agent.first_name?.trim().toLowerCase() === firstname.trim().toLowerCase() &&
-            agent.last_name?.trim().toLowerCase() === lastname.trim().toLowerCase()
-        );
+            agent.first_name?.trim().toLowerCase() ===
+              firstname.trim().toLowerCase() &&
+            agent.last_name?.trim().toLowerCase() ===
+              lastname.trim().toLowerCase()
+        )
 
         // Only fetch agent properties if matchedAgent exists
         //https://staging.cms.black-diamondz.62dev.org/items/agent_properties?fields=property_id.*.*&filter[user_id][_eq]=d936be1f-638e-42be-b6cd-eccc8dc4e8be ⁠
-        let dataAgentProperties = null;
+        let dataAgentProperties = null
+        let dataTestimonials = null
         if (matchedAgent && matchedAgent.id) {
           dataAgentProperties = await getItems("agent_properties", {
-            fields: ["property_id.*.*"],
+            fields: [
+              "property_id.*.*",
+              "property_id.type.translations.*",
+              "property_id.images.directus_files_id.*",
+            ],
             filter: {
               user_id: {
                 _eq: matchedAgent.id,
               },
             },
-          });
+          })
+
+          dataTestimonials = await getItems("aboutUs_team_testimonials", {
+            fields: ["testimonials.*.*"],
+            filter: {
+              user_id: {
+                id: { _eq: matchedAgent.id },
+              },
+            },
+          })
         }
-
-        setAgentData(matchedAgent || null);
-        setAgentProperties(dataAgentProperties);
-        setLoading(false);
+        const properties = dataAgentProperties.map((item) => item.property_id)
+        setAgentData(matchedAgent || null)
+        setAgentProperties(properties)
+        setTestimonials(dataTestimonials?.[0]?.testimonials || [])
+        setLoading(false)
       } catch (err) {
-        setError("Failed to load home data:" + err.message);
+        setError("Failed to load home data:" + err.message)
       }
-    };
-    fetchDataAgent();
-    
-  }, []);
+    }
+    fetchDataAgent()
+  }, [])
 
-  
   return (
     <>
       {loading ? (
@@ -68,9 +82,12 @@ export default function TeamMemberPage() {
           <Loading error={error} />
         </section>
       ) : (
-        
-        <TeamDetail member={agentData} agentProperties={agentProperties} />
+        <TeamDetail
+          member={agentData}
+          agentProperties={agentProperties}
+          testimonials={testimonials}
+        />
       )}
     </>
-  );
+  )
 }
